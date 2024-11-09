f2898d13c5eca75b88a63ae0df072e04e0c24f6e
Alisson Andrade Nunes <77127558+AlissoNNunes1@users.noreply.github.com>
2024-11-08 21:20:16 -0300

    Add character images to chapters and update settings for media files

--- game/models.py
+++ game/models.py
@@ -3,14 +3,20 @@ from django.db import models
 class Character(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    reputation = models.IntegerField(default=0)
+    image = models.ImageField(upload_to='images/', default='images/default.jpg')
 
     def __str__(self):
         return self.name
+
+    def get_reputation(self):
+        return self.reputation
 
 class Chapter(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     next_chapter = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
+    character = models.ForeignKey(Character, on_delete=models.CASCADE, related_name='chapters')
 
     def __str__(self):
         return self.title
