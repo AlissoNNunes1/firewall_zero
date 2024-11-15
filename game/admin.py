f36fdfe9ec8ebfe34f4345112d97ef5d89f1f4ae
Alisson Andrade Nunes <77127558+AlissoNNunes1@users.noreply.github.com>
2024-11-15 12:47:09 -0300

    feat(game): add chapter number and update related templates and views

--- game/admin.py
+++ game/admin.py
@@ -4,10 +4,11 @@ from .models import Character, Chapter, Choice
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 1
-    fk_name = 'chapter'  
+    fk_name = 'chapter'
 
 class ChapterAdmin(admin.ModelAdmin):
-    list_display = ('title', 'next_chapter')
+    list_display = ('title', 'num', 'next_chapter')
+    fields = ['title', 'num', 'content', 'next_chapter', 'character']
     inlines = [ChoiceInline]
 
 admin.site.register(Character)
--- game/urls.py
+++ game/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home_view'),
-    path('chapter/<int:chapter_id>/', views.chapter_view, name='chapter_view'),
+    path('chapter/<int:chapter_num>/', views.chapter_view, name='chapter_view'),
     path('chapters/', views.chapter_list, name='chapter_list'),
     path('add_chapter/', views.add_chapter, name='add_chapter'),
     path('settings/', views.settings, name='settings'),
